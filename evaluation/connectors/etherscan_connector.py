ec82255f828a177cd3ddf57680b2629b7cc1bb95
Fabian Galm <fabian.galm@gmail.com>
2023-03-07 15:07:30 +0100

    created etherscan connector

---
+++ evaluation/connectors/etherscan_connector.py
@@ -0,0 +1,53 @@
+import requests
+
+
+class EtherscanConnector:
+
+    def __init__(self, etherscan_ip: str, etherscan_api_key: str) -> None:
+        self.client_ip = etherscan_ip
+        self.api_key = etherscan_api_key
+
+    def get_block(self, block_identifier, full_transactions=False):
+        # type conveserion for hex
+        if type(block_identifier) is str:
+            block_identifier = int(block_identifier, 0)
+
+        params = {
+            "module": "proxy",
+            "action": "eth_getBlockByNumber",
+            "tag":  hex(block_identifier),
+            "boolean": full_transactions,
+            "apikey": self.api_key
+        }
+
+        response = requests.get(self.client_ip, params=params)
+
+        return response.json()
+
+    def get_transaction(self, transaction_hash: str):
+        params = {
+            "module": "proxy",
+            "action": "eth_getTransactionByHash",
+            "txhash":  transaction_hash,
+            "apikey": self.api_key
+        }
+
+        response = requests.get(self.client_ip, params=params)
+
+        return response.json()
+
+    def get_block_transaction_count(self, block_identifier):
+        # type conveserion for hex
+        if type(block_identifier) is str:
+            block_identifier = int(block_identifier, 0)
+
+        params = {
+            "module": "proxy",
+            "action": "eth_getBlockTransactionCountByNumber",
+            "tag":  hex(block_identifier),
+            "apikey": self.api_key
+        }
+
+        response = requests.get(self.client_ip, params=params)
+
+        return response.json()
