06869b6fcd1360dd6f385d860cc540e7bf7ee902
Fabian Galm <fabian.galm@gmail.com>
2023-03-08 12:59:46 +0100

    added wallet addess sample generation

--- evaluation/sample_generation.py
+++ evaluation/sample_generation.py
@@ -4,11 +4,16 @@ import random
 import json
 
 # params
+sample_folder = "evaluation/data_samples"
+
 generate_block_sample = False
 block_sample_size = 10
 
 generate_transaction_sample = False
 transaction_sample_size = 10
+
+generate_wallet_address_sample = True
+wallet_address_sample_size = 10
 
 latest_block = 16777791
 
@@ -46,13 +51,41 @@ def generate_random_transaction_sample(latest_block: int, sample_size: int, file
         outfile.write(sample_json)
 
 
+def generate_random_wallet_address_sample(latest_block: int, sample_size: int, file_path: str):
+    block_sample = random.sample(range(0, latest_block+1), sample_size)
+
+    wallet_address_sample = []
+
+    # get a single transaction from each random block and get from address from the transaction
+    for block_identifier in block_sample:
+        block = moralis_connector.get_block(block_identifier, False)
+
+        transaction_hash = random.choice(block["transactions"])
+
+        transaction = moralis_connector.get_transaction(transaction_hash)
+
+        wallet_address = transaction["from_address"]
+
+        wallet_address_sample.append(wallet_address)
+
+    sample_json = json.dumps(wallet_address_sample, indent=4)
+
+    with open(file_path, "w") as outfile:
+        outfile.write(sample_json)
+
+
 if __name__ == "__main__":
-    # generate sample
+    # generate block sample
     if generate_block_sample:
         generate_random_block_sample(
-            latest_block, block_sample_size, "evaluation/data_samples/block_sample.json")
+            latest_block, block_sample_size, f"{sample_folder}/block_sample.json")
 
-    # generate sample
+    # generate block sample
     if generate_transaction_sample:
         generate_random_transaction_sample(
-            latest_block, transaction_sample_size, "evaluation/data_samples/transaction_sample.json")
+            latest_block, transaction_sample_size, f"{sample_folder}/transaction_sample.json")
+
+    # generate address sample
+    if generate_wallet_address_sample:
+        generate_random_wallet_address_sample(
+            latest_block, wallet_address_sample_size, f"{sample_folder}/wallet_address_sample.json")
