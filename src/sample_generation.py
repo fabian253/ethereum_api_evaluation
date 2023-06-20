5b4f67409e0eac69fb4de76e5d1ab12b2c939db3
Fabian Galm <fabian.galm@gmail.com>
2023-06-20 17:02:35 +0200

    updated

--- src/sample_generation.py
+++ src/sample_generation.py
@@ -10,8 +10,8 @@ sample_folder = "src/data_samples"
 block_sample_name = "block_sample"
 transaction_sample_name = "transaction_sample"
 wallet_address_sample_name = "wallet_address_sample"
-early_late_block_sample_name = "early_late_block_sample"
-early_late_transaction_sample_name = "early_late_transaction_sample"
+timeframe_block_sample_name = "timeframe_block_sample"
+timeframe_transaction_sample_name = "timeframe_transaction_sample"
 
 generate_block_sample = False
 block_sample_size = 1000
@@ -22,35 +22,36 @@ transaction_sample_size = 1000
 generate_wallet_address_sample = True
 wallet_address_sample_size = 1000
 
-generate_early_late_block_sample = False
-early_late_block_sample_size = 1000
+generate_timeframe_block_sample = False
+timeframe_block_sample_size = 1000
 
-generate_early_late_transaction_sample = True
-early_late_transaction_sample_size = 1000
+generate_timeframe_transaction_sample = True
+timeframe_transaction_sample_size = 1000
 
 earliest_block = 0
-latest_block = 17000000
+newst_block = 17000000
 
-early_sample_earliest_block = 1000000
-early_sample_latest_block = 2000000
+old_sample_earliest_block = 1000000
+old_sample_newst_block = 2000000
 
-late_sample_earliest_block = 16000000
-late_sample_latest_block = 17000000
+new_sample_earliest_block = 16000000
+new_sample_newst_block = 17000000
 
 
 # create moralis connector
-infura_connector = InfuraConnector(connector_config.INFURA_IP, connector_config.INFURA_API_KEY)
+infura_connector = InfuraConnector(
+    connector_config.INFURA_IP, connector_config.INFURA_API_KEY)
 
 
-def generate_random_block_sample(earliest_block: int, latest_block: int, sample_size: int):
-    sample = random.sample(range(earliest_block, latest_block+1), sample_size)
+def generate_random_block_sample(earliest_block: int, newst_block: int, sample_size: int):
+    sample = random.sample(range(earliest_block, newst_block+1), sample_size)
 
     return sample
 
 
-def generate_random_transaction_sample(earliest_block: int, latest_block: int, sample_size: int):
+def generate_random_transaction_sample(earliest_block: int, newst_block: int, sample_size: int):
     block_sample = random.sample(
-        range(earliest_block, latest_block+1), sample_size)
+        range(earliest_block, newst_block+1), sample_size)
 
     transaction_sample = []
 
@@ -68,16 +69,16 @@ def generate_random_transaction_sample(earliest_block: int, latest_block: int, s
             new_block_identifier = block_sample[0]
             while new_block_identifier in block_sample:
                 new_block_identifier = random.randint(
-                    earliest_block, latest_block+1)
+                    earliest_block, newst_block+1)
 
             block_sample.append(new_block_identifier)
 
     return transaction_sample
 
 
-def generate_random_wallet_address_sample(earliest_block: int, latest_block: int, sample_size: int):
+def generate_random_wallet_address_sample(earliest_block: int, newst_block: int, sample_size: int):
     transaction_sample = generate_random_transaction_sample(
-        earliest_block, latest_block, sample_size)
+        earliest_block, newst_block, sample_size)
 
     wallet_address_sample = []
 
@@ -94,27 +95,27 @@ def generate_random_wallet_address_sample(earliest_block: int, latest_block: int
     return wallet_address_sample
 
 
-def generate_random_early_late_block_sample(early_sample_earliest_block: int, early_sample_latest_block: int, late_sample_earliest_block: int, late_sample_latest_block: int, sample_size: int):
-    early_block_sample = generate_random_block_sample(
-        early_sample_earliest_block, early_sample_latest_block, sample_size)
-    late_block_sample = generate_random_block_sample(
-        late_sample_earliest_block, late_sample_latest_block, sample_size)
+def generate_random_timeframe_block_sample(old_sample_earliest_block: int, old_sample_newst_block: int, new_sample_earliest_block: int, new_sample_newst_block: int, sample_size: int):
+    old_block_sample = generate_random_block_sample(
+        old_sample_earliest_block, old_sample_newst_block, sample_size)
+    new_block_sample = generate_random_block_sample(
+        new_sample_earliest_block, new_sample_newst_block, sample_size)
 
     return {
-        "early_block_sample": early_block_sample,
-        "late_block_sample": late_block_sample
+        "old_block_sample": old_block_sample,
+        "new_block_sample": new_block_sample
     }
 
 
-def generate_random_early_late_transaction_sample(early_sample_earliest_block: int, early_sample_latest_block: int, late_sample_earliest_block: int, late_sample_latest_block: int, sample_size: int):
-    early_transaction_sample = generate_random_transaction_sample(
-        early_sample_earliest_block, early_sample_latest_block, sample_size)
-    late_transaction_sample = generate_random_transaction_sample(
-        late_sample_earliest_block, late_sample_latest_block, sample_size)
+def generate_random_timeframe_transaction_sample(old_sample_earliest_block: int, old_sample_newst_block: int, new_sample_earliest_block: int, new_sample_newst_block: int, sample_size: int):
+    old_transaction_sample = generate_random_transaction_sample(
+        old_sample_earliest_block, old_sample_newst_block, sample_size)
+    new_transaction_sample = generate_random_transaction_sample(
+        new_sample_earliest_block, new_sample_newst_block, sample_size)
 
     return {
-        "early_transaction_sample": early_transaction_sample,
-        "late_transaction_sample": late_transaction_sample
+        "old_transaction_sample": old_transaction_sample,
+        "new_transaction_sample": new_transaction_sample
     }
 
 
@@ -129,7 +130,7 @@ if __name__ == "__main__":
     # generate block sample
     if generate_block_sample:
         sample = generate_random_block_sample(
-            earliest_block, latest_block, block_sample_size)
+            earliest_block, newst_block, block_sample_size)
 
         write_sample_to_json(sample, sample_folder, block_sample_name)
         print("Block sample generated")
@@ -137,7 +138,7 @@ if __name__ == "__main__":
     # generate transaction sample
     if generate_transaction_sample:
         transaction_sample = generate_random_transaction_sample(
-            earliest_block, latest_block, transaction_sample_size)
+            earliest_block, newst_block, transaction_sample_size)
 
         write_sample_to_json(transaction_sample,
                              sample_folder, transaction_sample_name)
@@ -146,26 +147,26 @@ if __name__ == "__main__":
     # generate address sample
     if generate_wallet_address_sample:
         wallet_address_sample = generate_random_wallet_address_sample(
-            earliest_block, latest_block, wallet_address_sample_size)
+            earliest_block, newst_block, wallet_address_sample_size)
 
         write_sample_to_json(wallet_address_sample,
                              sample_folder, wallet_address_sample_name)
         print("Wallet Address sample generated")
 
-    # generate early-late block sample
-    if generate_early_late_block_sample:
-        early_late_block_sample = generate_random_early_late_block_sample(
-            early_sample_earliest_block, early_sample_latest_block, late_sample_earliest_block, late_sample_latest_block, early_late_block_sample_size)
+    # generate old-new block sample
+    if generate_timeframe_block_sample:
+        timeframe_block_sample = generate_random_timeframe_block_sample(
+            old_sample_earliest_block, old_sample_newst_block, new_sample_earliest_block, new_sample_newst_block, timeframe_block_sample_size)
 
-        write_sample_to_json(early_late_block_sample,
-                             sample_folder, early_late_block_sample_name)
-        print("Early Late Block sample generated")
+        write_sample_to_json(timeframe_block_sample,
+                             sample_folder, timeframe_block_sample_name)
+        print("Timeframe Block sample generated")
 
-    # generate early-late transaction sample
-    if generate_early_late_transaction_sample:
-        early_late_transaction_sample = generate_random_early_late_transaction_sample(
-            early_sample_earliest_block, early_sample_latest_block, late_sample_earliest_block, late_sample_latest_block, early_late_transaction_sample_size)
+    # generate old-new transaction sample
+    if generate_timeframe_transaction_sample:
+        timeframe_transaction_sample = generate_random_timeframe_transaction_sample(
+            old_sample_earliest_block, old_sample_newst_block, new_sample_earliest_block, new_sample_newst_block, timeframe_transaction_sample_size)
 
-        write_sample_to_json(early_late_transaction_sample,
-                             sample_folder, early_late_transaction_sample_name)
-        print("Early Late Transaction sample generated")
+        write_sample_to_json(timeframe_transaction_sample,
+                             sample_folder, timeframe_transaction_sample_name)
+        print("Timeframe Transaction sample generated")
