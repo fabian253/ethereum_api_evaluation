aa6c27eb2b51ec307394449217168bc950b3535a
Fabian Galm <fabian.galm@gmail.com>
2023-06-20 17:02:31 +0200

    updated

--- src/main.py
+++ src/main.py
@@ -2,12 +2,25 @@ import evalutation_config as config
 from evaluation.evaluation_controller import EvaluationController
 from data_sample import *
 
+old_block_sample = timeframe_block_sample["old_block_sample"][:2]
+new_block_sample = timeframe_block_sample["new_block_sample"][:2]
+
+timeframe_block_sample["old_block_sample"] = old_block_sample
+timeframe_block_sample["new_block_sample"] = new_block_sample
+
+old_transaction_sample = timeframe_transaction_sample["old_transaction_sample"][:2]
+new_transaction_sample = timeframe_transaction_sample["new_transaction_sample"][:2]
+
+timeframe_transaction_sample["old_transaction_sample"] = old_transaction_sample
+timeframe_transaction_sample["new_transaction_sample"] = new_transaction_sample
+
+
 evaluation_controller = EvaluationController(
-    block_sample,
-    transaction_sample,
-    wallet_address_sample,
-    early_late_block_sample,
-    early_late_transaction_sample
+    block_sample[:2],
+    transaction_sample[:2],
+    wallet_address_sample[:2],
+    timeframe_block_sample,
+    timeframe_transaction_sample
 )
 
 
@@ -19,3 +32,7 @@ if __name__ == "__main__":
     # evaluate heuristic
     evaluation_controller.evaluate_heuristic(
         config.HEURISTIC_EVALUATE_TRANSACTION_COUNT, config.HEURISTIC_EVALUATE_WALLET_BALANCE, config.HEURISTIC_FILE_PATH)
+
+    # evaluate performance
+    evaluation_controller.evaluate_performance(
+        config.PERFORMANCE_EVALUATE_API_PROVIDER, config.PERFORMANCE_EVALUATE_TIMEFRAME, config.PERFORMANCE_FILE_PATH)
