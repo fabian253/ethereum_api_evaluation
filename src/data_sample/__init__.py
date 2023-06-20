4ac9b7f58993e09db32a8500c994d8538f0a54a2
Fabian Galm <fabian.galm@gmail.com>
2023-06-20 17:02:52 +0200

    updated

--- src/data_sample/__init__.py
+++ src/data_sample/__init__.py
@@ -1,2 +1,2 @@
 # __init__.py
-from .sample_init import block_sample, transaction_sample, wallet_address_sample, early_late_block_sample, early_late_transaction_sample
+from .sample_init import block_sample, transaction_sample, wallet_address_sample, timeframe_block_sample, timeframe_transaction_sample
--- src/data_sample/sample_init.py
+++ src/data_sample/sample_init.py
@@ -9,8 +9,8 @@ with open("src/data_sample/transaction_sample.json", "r") as infile:
 with open("src/data_sample/wallet_address_sample.json", "r") as infile:
     wallet_address_sample = json.load(infile)
 
-with open("src/data_sample/early_late_block_sample.json", "r") as infile:
-    early_late_block_sample = json.load(infile)
+with open("src/data_sample/timeframe_block_sample.json", "r") as infile:
+    timeframe_block_sample = json.load(infile)
 
-with open("src/data_sample/early_late_transaction_sample.json", "r") as infile:
-    early_late_transaction_sample = json.load(infile)
+with open("src/data_sample/timeframe_transaction_sample.json", "r") as infile:
+    timeframe_transaction_sample = json.load(infile)
