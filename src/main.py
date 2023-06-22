6b40f30f8ce4ce8cd5ae5cd857fc96154dfc43da
Fabian Galm <fabian.galm@gmail.com>
2023-06-22 12:15:23 +0200

    updated eval

--- src/main.py
+++ src/main.py
@@ -27,12 +27,38 @@ evaluation_controller = EvaluationController(
 if __name__ == "__main__":
     # evaluate data correctness
     evaluation_controller.evaluate_data_correctness(
-        config.DATA_CORRECTNESS_EVALUATE_BLOCK_CORRECTNESS, config.DATA_CORRECTNESS_EVALUATE_TRANSACTION_CORRECTNESS, config.DATA_CORRECTNESS_FILE_PATH)
+        config.DATA_CORRECTNESS_EVALUATE_BLOCK_CORRECTNESS,
+        config.DATA_CORRECTNESS_EVALUATE_TRANSACTION_CORRECTNESS,
+        config.DATA_CORRECTNESS_FILE_PATH)
 
     # evaluate heuristic
     evaluation_controller.evaluate_heuristic(
-        config.HEURISTIC_EVALUATE_TRANSACTION_COUNT, config.HEURISTIC_EVALUATE_WALLET_BALANCE, config.HEURISTIC_FILE_PATH)
+        config.HEURISTIC_EVALUATE_TRANSACTION_COUNT,
+        config.HEURISTIC_EVALUATE_WALLET_BALANCE,
+        config.HEURISTIC_FILE_PATH)
 
     # evaluate performance
     evaluation_controller.evaluate_performance(
-        config.PERFORMANCE_EVALUATE_API_PROVIDER, config.PERFORMANCE_EVALUATE_TIMEFRAME, config.PERFORMANCE_FILE_PATH)
+        config.PERFORMANCE_EVALUATE_API_PROVIDER,
+        config.PERFORMANCE_EVALUATE_TIMEFRAME,
+        config.PERFORMANCE_FILE_PATH)
+
+    # evaluate research question token evolution
+    evaluation_controller.evaluate_research_question_token_evolution(
+        config.RESEARCH_QUESTION_TOKEN_EVOLUTION_DESCRIPTIVE,
+        config.RESEARCH_QUESTION_TOKEN_EVOLUTION_CONTRACT_ADDRESS,
+        config.RESEARCH_QUESTION_TOKEN_EVOLUTION_TOKEN_ID,
+        config.RESEARCH_QUESTION_TOKEN_EVOLUTION_FILE_PATH)
+
+    # evaluate research question contract evolution
+    evaluation_controller.evaluate_research_question_contract_evolution(
+        config.RESEARCH_QUESTION_CONTRACT_EVOLUTION,
+        config.RESEARCH_QUESTION_CONTRACT_EVOLUTION_FILE_PATH
+    )
+
+    # evaluate research question dao contract evolution
+    evaluation_controller.evaluate_research_question_dao_contract_evolution(
+        config.RESEARCH_QUESTION_DAO_CONTRACT_EVOLUTION,
+        config.RESEARCH_QUESTION_DAO_CONTRACT_EVOLUTION_CONTRACT_ADDRESS,
+        config.RESEARCH_QUESTION_DAO_CONTRACT_EVOLUTION_FILE_PATH
+    )
