c694df872549524637b6cb6912264988080f74fd
Fabian Galm <fabian.galm@gmail.com>
2023-06-22 22:58:27 +0200

    added new sample

--- src/data_sample/__init__.py
+++ src/data_sample/__init__.py
@@ -1,2 +1,2 @@
 # __init__.py
-from .sample_init import block_sample, transaction_sample, wallet_address_sample, timeframe_block_sample, timeframe_transaction_sample
+from .sample_init import block_sample, transaction_sample, wallet_address_sample, timeframe_block_sample, timeframe_transaction_sample, contract_address_sample
--- src/data_sample/sample_init.py
+++ src/data_sample/sample_init.py
@@ -1,16 +1,19 @@
 import json
 
-with open("src/data_sample/block_sample.json", "r") as infile:
-    block_sample = json.load(infile)
+with open("src/data_sample/block_sample.json", "r") as f:
+    block_sample = json.load(f)
 
-with open("src/data_sample/transaction_sample.json", "r") as infile:
-    transaction_sample = json.load(infile)
+with open("src/data_sample/transaction_sample.json", "r") as f:
+    transaction_sample = json.load(f)
 
-with open("src/data_sample/wallet_address_sample.json", "r") as infile:
-    wallet_address_sample = json.load(infile)
+with open("src/data_sample/wallet_address_sample.json", "r") as f:
+    wallet_address_sample = json.load(f)
 
-with open("src/data_sample/timeframe_block_sample.json", "r") as infile:
-    timeframe_block_sample = json.load(infile)
+with open("src/data_sample/timeframe_block_sample.json", "r") as f:
+    timeframe_block_sample = json.load(f)
 
-with open("src/data_sample/timeframe_transaction_sample.json", "r") as infile:
-    timeframe_transaction_sample = json.load(infile)
+with open("src/data_sample/timeframe_transaction_sample.json", "r") as f:
+    timeframe_transaction_sample = json.load(f)
+
+with open("src/data_sample/contract_address_sample.json", "r") as f:
+    contract_address_sample = json.load(f)
